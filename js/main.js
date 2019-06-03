cefd95d5da86cf0ac8cc4b2394e58411454a635f
Cristopher Sazon <csazon@buildzoom.com>
2019-06-04 06:41:36 +0800

    played on ajax request

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// api: https://api.github.com/users/<username>
+var ajaxRequest = new XMLHttpRequest();
+ajaxRequest.open('GET', 'https://api.github.com/users/cristophersazon');
+ajaxRequest.onreadystatechange = function() {
+	console.log(this.status);
+	console.log(ajaxRequest.responseText);
+}
+
+
+
 function sendMessage() {
 	let submitResult = document.getElementById('submitResult');
 	let nameInput = document.getElementById('nameInput');
@@ -6,16 +16,21 @@ function sendMessage() {
 	let messageInput = document.getElementById('messageInput');
 	if (!nameInput.value || !phoneInput.value || !emailInput.value || !messageInput.value) {
 		submitResult.innerHTML = 'Message not sent. Other fields are empty.'		
+		submitResult.style.color = 'red'
+		ajaxRequest.send();
 	} else {
 		nameInput.value = ''
 		phoneInput.value = ''
 		emailInput.value = ''
 		messageInput.value = ''
 		submitResult.innerHTML = 'Message Sent!'
+		submitResult.style.color = '#fff';
 	}
 };
 
-function nameOnFocus() {
+function fieldOnFocus() {
 	let submitResult = document.getElementById('submitResult');
 	submitResult.innerHTML = ''
 }
+
+
